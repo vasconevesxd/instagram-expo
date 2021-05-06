ad53ac7284839dd0754ef9b7c357eae9b6edbc6b
Vasco Neves <57102041+vasconevesxd@users.noreply.github.com>
2021-05-06 14:25:56 +0100

    Likes and Comments

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import React, { Component } from "react";
-import { View, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./redux/reducers";
-import thunk from "redux-thunk";
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native'
+
+import * as firebase from 'firebase'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
 import config from './config'
-// This import loads the firebase namespace.
-import firebase from "firebase/app";
 
-// These imports load individual services into the firebase namespace.
-import "firebase/auth";
-import "firebase/database";
-import "firebase/firestore";
-import "firebase/storage";
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const firebaseConfig = {
   apiKey: config.API_Key,
@@ -30,61 +23,63 @@ const firebaseConfig = {
   measurementId: config.MEASUREMENT_ID,
 };
 
-if (firebase.default.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
 }
 
-import LandingScreen from "./components/auth/Landing";
-import RegisterScreen from "./components/auth/Register";
-import LoginScreen from "./components/auth/Login";
-import MainScreen from "./components/Main";
-import AddScreen from "./components/main/Add";
-import SaveScreen from "./components/main/Save";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
+import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
+import CommentScreen from './components/main/Comment'
+
 
 const Stack = createStackNavigator();
 
+
 export class App extends Component {
   constructor(props) {
-    super(props);
+    super()
     this.state = {
       loaded: false,
-    };
+    }
   }
-
-
+ 
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (!user) {
         this.setState({
           loggedIn: false,
           loaded: true,
-        });
+        })
       } else {
         this.setState({
           loggedIn: true,
           loaded: true,
-        });
+        })
       }
-    });
+    })
   }
   render() {
     const { loggedIn, loaded } = this.state;
     if (!loaded) {
       return (
-        <View style={{ flex: 1, justifyContent: "center" }}>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
           <Text>Loading</Text>
         </View>
-      );
+      )
     }
+
     if (!loggedIn) {
       return (
         <NavigationContainer>
-          <Stack.Navigator initialROuteName="Landing">
-            <Stack.Screen
-              name="Landing"
-              component={LandingScreen}
-              options={{ headerShown: false }}
-            />
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
             <Stack.Screen name="Register" component={RegisterScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
           </Stack.Navigator>
@@ -94,20 +89,17 @@ export class App extends Component {
 
     return (
       <Provider store={store}>
-        <NavigationContainer>
-          <Stack.Navigator initialROuteName="Main">
-            <Stack.Screen
-              name="Main"
-              component={MainScreen}
-
-            />
-            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
-            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation} />
+        <NavigationContainer >
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Comment" component={CommentScreen} navigation={this.props.navigation}/>
           </Stack.Navigator>
         </NavigationContainer>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,10 +1,21 @@
-import firebase from "firebase";
 import {
   USER_STATE_CHANGE,
   USER_POSTS_STATE_CHANGE,
   USER_FOLLOWING_STATE_CHANGE,
+  USERS_DATA_STATE_CHANGE,
+  USERS_POSTS_STATE_CHANGE,
+  USERS_LIKES_STATE_CHANGE,
+  CLEAR_DATA,
 } from "../constants/index";
+import firebase from "firebase";
 
+require("firebase/firestore");
+
+export function clearData() {
+  return (dispatch) => {
+    dispatch({ type: CLEAR_DATA });
+  };
+}
 export function fetchUser() {
   return (dispatch) => {
     firebase
@@ -55,6 +66,84 @@ export function fetchUserFollowing() {
           return id;
         });
         dispatch({ type: USER_FOLLOWING_STATE_CHANGE, following });
+        for (let i = 0; i < following.length; i++) {
+          dispatch(fetchUsersData(following[i], true));
+        }
+      });
+  };
+}
+
+export function fetchUsersData(uid, getPosts) {
+  return (dispatch, getState) => {
+    const found = getState().usersState.users.some((el) => el.uid === uid);
+    if (!found) {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(uid)
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists) {
+            let user = snapshot.data();
+            user.uid = snapshot.id;
+
+            dispatch({ type: USERS_DATA_STATE_CHANGE, user });
+          } else {
+            console.log("does not exist");
+          }
+        });
+      if (getPosts) {
+        dispatch(fetchUsersFollowingPosts(uid));
+      }
+    }
+  };
+}
+
+export function fetchUsersFollowingPosts(uid) {
+  return (dispatch, getState) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(uid)
+      .collection("userPosts")
+      .orderBy("creation", "asc")
+      .get()
+      .then((snapshot) => {
+        const uid = snapshot.docs[0].ref.path.split("/")[1];
+        const user = getState().usersState.users.find((el) => el.uid === uid);
+   
+        let posts = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          const id = doc.id;
+          return { id, ...data, user };
+        });
+        for (let i = 0; i < posts.length; i++) {
+          dispatch(fetchUsersFollowingLikes(uid, posts[i].id));
+        }
+        dispatch({ type: USERS_POSTS_STATE_CHANGE, posts, uid });
+      });
+  };
+}
+
+export function fetchUsersFollowingLikes(uid, postId) {
+  return (dispatch, getState) => {
+    firebase
+      .firestore()
+      .collection("posts")
+      .doc(uid)
+      .collection("userPosts")
+      .doc(postId)
+      .collection("likes")
+      .doc(firebase.auth().currentUser.uid)
+      .onSnapshot((snapshot) => {
+        const postId = snapshot.ZE.path.segments[3];
+
+        let currentUserLike = false;
+        if (snapshot.exists) {
+          currentUserLike = true;
+        }
+
+        dispatch({ type: USERS_LIKES_STATE_CHANGE, postId, currentUserLike });
       });
   };
 }
